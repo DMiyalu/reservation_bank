84db884e911be6607397a2d701d1b95c357668c2
DMiyalu <dieudonnemiyalu6@gmail.com>
2021-06-17 19:18:29 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(request, response) {
+    response.render('pages/index');
+});
+
+app.get('/contact', function(request, response) {
+    response.render('pages/contact');
+});
+
+app.listen(8080);
+console.log('8080 is the magic port');

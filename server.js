88cb5eb765170f31ea8acb1a93bf5e4eb2d31aec
DMiyalu <dieudonnemiyalu6@gmail.com>
2021-06-18 09:56:06 +0100

    retrait depot frais-acedemaique

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const { body,validationResult } = require('express-validator');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -19,12 +20,28 @@ app.get('/depot', function(request, response) {
     response.render('pages/depot');
 });
 
+app.post('/depot', function(request, response) {
+    response.redirect('/jeton');
+});
+
 app.get('/retrait', function(request, response) {
     response.render('pages/retrait');
 });
 
+app.post('/retrait', function(request, response) {
+    response.redirect('/jeton');
+});
+
+app.get('/jeton', function(request, response) {
+    response.render('pages/jeton');
+});
+
 app.get('/frais-academiques', function(request, response) {
     response.render('pages/frais-academiques');
+});
+
+app.post('/frais-academiques', function(request, response) {
+    response.redirect('/jeton');
 });
 
 app.listen(8080);
